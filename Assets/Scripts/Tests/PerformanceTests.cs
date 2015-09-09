49cfe7e17f8df2c2b7e9bb9f3f91d82322729970
dongbo <dbdongbo@vip.qq.com>
2015-09-09 15:56:29 +0800

    performance tests component.

---
+++ Assets/Scripts/Tests/PerformanceTests.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Assets.Scripts.Common;
+
+public class PerformanceTests : MonoBehaviour
+{
+    List<int> Tests = new List<int>();
+
+    void Awake()
+    {
+        Tests.Add(100);
+        Tests.Add(1000);
+    }
+    void Update()
+    {
+        SysTests();
+        CustomTests();
+    }
+
+    void SysTests()
+    {
+        var N = new System.Collections.ObjectModel.ReadOnlyCollection<int>(Tests);
+
+        var Iter = N.GetEnumerator();
+
+        while( Iter.MoveNext() )
+        {
+
+        }
+    }
+
+    void CustomTests()
+    {
+        var N = new ReadonlyContext<int>(Tests);
+
+        var Iter = N.GetEnumerator();
+
+        while (Iter.MoveNext())
+        {
+
+        }
+    }    
+}
